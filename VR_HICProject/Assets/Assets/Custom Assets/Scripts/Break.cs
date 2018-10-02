d334b376ca27169a032954984dfc831987979825
Arniox <nikkdiehl@gmail.com>
2018-10-03 00:26:05 +1300

    finished everything

---
+++ VR_HICProject/Assets/Assets/Custom Assets/Scripts/Break.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Break : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision col){
+		gameObject.GetComponent<Rigidbody>().useGravity = true;
+
+		if(gameObject.GetComponent<Rigidbody>().useGravity == true){
+			print("gave gravity");
+		}
+		
+	}
+}
