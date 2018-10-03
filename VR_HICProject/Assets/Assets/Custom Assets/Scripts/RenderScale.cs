3c3e0f727341ae5a9e78b2d6a4edc056287dd021
Arniox <nikkdiehl@gmail.com>
2018-10-03 16:59:11 +1300

    Added VR Control

---
+++ VR_HICProject/Assets/Assets/Custom Assets/Scripts/RenderScale.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class RenderScale : MonoBehaviour {
+
+    [SerializeField] private float m_RenderScale = 1.5f;
+
+	// Use this for initialization
+	void Start () {
+        XRSettings.eyeTextureResolutionScale = m_RenderScale;
+	}
+}
